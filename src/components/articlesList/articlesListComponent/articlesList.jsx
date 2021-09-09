06a8c91b3728d4cd9ce373a9dd8650f334b91f32
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2021-09-09 12:40:10 -0500

    added premium tag to article cards

--- src/components/articlesList/articlesListComponent/articlesList.jsx
+++ src/components/articlesList/articlesListComponent/articlesList.jsx
@@ -1,50 +1,17 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
-import Link from 'next/link';
-import styles from './articlesList.module.css';
-import { BUCKET_URL } from '@/global/constants';
+import ArticlesDetailComponent from './articleDetall';
 
 const ArticlesListComponent = ({ articles }) => {
-  const getTypeIcon = (type) => {
-    switch (type) {
-      case 'Video': return 'F';
-      case 'Podcast': return 'Q';
-      default: return 'P';
-    }
-  };
-
   return (
     <div>
       {articles.map((article) => {
         return (
-          <Link
+          <ArticlesDetailComponent
             key={article._id}
-            href={`/trending-topics/${article.usuario_id.slug}/${article.slug}`}
-            prefetch={false}
-            passHref
-          >
-            <div className={styles.cardContainer}>
-              <div className={styles.cardImageContainer}>
-                <div className={`main-bg text-sm text--theme-light ${styles.trendingLabel}`}>
-                  {article.tipo}{' '}<span className="icon text--theme-light">{getTypeIcon(article.tipo)}</span>
-                </div>
-                <img
-                  src={article.portada.ruta_imagen ? `${BUCKET_URL}${article.portada.ruta_imagen}` : '/images/imgpr2.jpg'}
-                  alt=""
-                />
-              </div>
-              <div className={styles.cardLikesContainer}>
-                <div className="text-sm">{article.usuario_id.name} {article.usuario_id.apellidos}</div>
-                <div className={`text-sm ${styles.likesContainer}`}><span className="icon">c</span>{' '}{article.likes}</div>
-              </div>
-              <div className={`title ${styles.cardMargin}`}>
-                {article.portada.titulo || 'Sin título'}
-              </div>
-              <div className="text-sm">
-                {article.portada.descripcion || 'Sin descripción'}
-              </div>
-            </div>
-          </Link>
+            article={article}
+            classContent=""
+          />
         );
       })}
     </div>
