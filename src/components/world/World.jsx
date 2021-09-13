6578dab4056d9492b670fea4a68b383009d461e1
Christian <christian@girasolo.com>
2021-09-13 21:28:55 +0000

    Feature imagen regiones

--- src/components/world/World.jsx
+++ src/components/world/World.jsx
@@ -1,22 +1,23 @@
-import Script from 'next/script';
-import React, { useEffect } from 'react';
-import define from './index';
-import { Runtime, Inspector } from './runtime';
+// import Script from 'next/script';
+import React from 'react';
+// import define from './index';
+// import { Runtime, Inspector } from './runtime';
 
 const WorldComponent = () => {
-  useEffect(() => {
-    const runtime = new Runtime();
-    runtime.module(define, Inspector.into(document.getElementById('body')));
-  }, []);
+  // useEffect(() => {
+  //   const runtime = new Runtime();
+  //   runtime.module(define, Inspector.into(document.getElementById('body')));
+  // }, []);
 
   return (
     <div id="chart">
       <div id="body" />
       <>
-        <Script
+        {/* <Script
           src="https://d3js.org/d3.v7.min.js"
           strategy="lazyOnload"
-        />
+        /> */}
+        <img className="img img-fluid" src="/images/home/Mapa.png" alt="impactgo geográfico" />
       </>
     </div>
   );
