cd66a66d49c8ee1d20b30c6d643fcba8f3ce708b
Abel Salazar <abel@girasolo.com>
2021-09-24 19:19:58 +0000

    Subs modal tools

--- src/components/thinkTools/accordionComponent/AccordionCollapse.jsx
+++ src/components/thinkTools/accordionComponent/AccordionCollapse.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 import { useSession } from 'next-auth/client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { reviewerAccess } from '@/helpers/accessVerifiers';
+import { SubscriptionModal } from '@/components';
 import DropDownOptions from './DropDownOptions';
+import styles from './accordion.module.css';
 
 const AccordionCollapse = ({ herramienta }) => {
   const [hover, setHover] = useState(false);
   const [session] = useSession();
-  const { user } = session;
+  const [show, setModal] = useState(false);
 
   const [optionsDropDown] = useState([
     {
@@ -26,14 +29,29 @@ const AccordionCollapse = ({ herramienta }) => {
     },
   ]);
 
+  useEffect(() => { }, [session]);
+
   return (
     <div className="row align-items-center justify-content-end mb-2" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
       <div className="col-8 offset-2  offset-md-2 col-sm-8 col-md-7">
-        <div className="text-md">{herramienta.name}</div>
-        <div className="text--theme-secondary text-md">{herramienta.tipo}</div>
+        {
+          (session) ? (
+            <Link href="/think-tools" passHref>
+              <a>
+                <div className="text-md">{herramienta.name}</div>
+                <div className="text--theme-secondary text-md">{herramienta.tipo}</div>
+              </a>
+            </Link>
+          ) : (
+            <div onClick={() => setModal(true)} className={styles.link_tool}>
+              <div className="text-md">{herramienta.name}</div>
+              <div className="text--theme-secondary text-md">{herramienta.tipo}</div>
+            </div>
+          )
+        }
       </div>
       <div className="col-2 col-sm-2 offset-md-1 col-md-2 justify-content-end">
-        {reviewerAccess(user.role) && (
+        {session && (reviewerAccess(session.user.role) && (
           <>
             {
               hover ? (
@@ -43,8 +61,9 @@ const AccordionCollapse = ({ herramienta }) => {
               )
             }
           </>
-        )}
+        ))}
       </div>
+      <SubscriptionModal show={show} setModal={setModal} />
     </div>
   );
 };
