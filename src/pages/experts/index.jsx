763c32cda37d2277f7bee38a89550f7cb881619c
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2021-10-08 22:20:55 +0000

    Feature experts search

--- src/pages/experts/index.jsx
+++ src/pages/experts/index.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { Footer, Layout } from '@/components';
 import ExpertsList from '@/components/experts/expertsList/ExpertsList';
-import { getExpertsSSR } from '@/services/user';
-// import { getExperts } from '@/services/user';
 
-const ExpertsPage = ({ initialData }) => {
+const ExpertsPage = () => {
   return (
     <Layout>
       <div className="container">
-        <ExpertsList initialData={initialData} />
+        <ExpertsList />
       </div>
       <Footer />
     </Layout>
   );
 };
-
-export async function getStaticProps() {
-  const res = await getExpertsSSR({ pageNum: 1 });
-  return {
-    props: {
-      initialData: res,
-    },
-    revalidate: 60,
-  };
-}
 
 export default ExpertsPage;
--- src/services/user.js
+++ src/services/user.js
@@ -2,21 +2,6 @@
 import { BASE_URL } from '@/global/constants';
 import axios from './axios';
 
-export const getExpertsSSR = async (params) => {
-  const { pageSize, pageNum } = params;
-  try {
-    const res = await axios().get('users/expertos', {
-      params: {
-        pageSize: pageSize || 8,
-        pageNum: pageNum || 1,
-      },
-    });
-    return res;
-  } catch (err) {
-    return err;
-  }
-};
-
 export const updateUserProfile = async (idUser, data) => {
   try {
     const res = await axios().patch(`${BASE_URL}users/profile/${idUser}`, { ...data });
