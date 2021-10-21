65fef63b8198522e10522b74f2e801b5cf583eec
Christian <christian@girasolo.com>
2021-10-21 14:44:26 -0500

    Ajuste responsive movil

--- src/components/home/NoArticles.jsx
+++ src/components/home/NoArticles.jsx
@@ -5,7 +5,7 @@ import styles from '@/global/styles/Home.module.css';
 export const NoArticles = () => {
   return (
     <Row className={styles.marginNoArticles}>
-      <div className={`${styles.content_centered} ${styles.content_more}`}>
+      <div className={`${styles.content_centered}`}>
         <p className="title">¡Bienvenido!</p>
         <Link href="trending-topics/" passHref>
           <a>
