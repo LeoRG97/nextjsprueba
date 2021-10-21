474ebfabe4acf63fbe7000444939956423583c92
Christian <christian@girasolo.com>
2021-10-21 13:13:32 -0500

    Imagen de fondo & componente reenderizable cuando no existen articulos destacados en Home

---
+++ src/components/home/NoArticles.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { Row } from 'react-bootstrap';
+import styles from '@/global/styles/Home.module.css';
+
+export const NoArticles = () => {
+  return (
+    <Row className={styles.marginNoArticles}>
+      <div className={`${styles.content_centered} ${styles.content_more}`}>
+        <p className="title">¡Bienvenido!</p>
+        <Link href="trending-topics/" passHref>
+          <a>
+            <button className="button button--theme-secondary">Explorar temas</button>
+          </a>
+        </Link>
+      </div>
+    </Row>
+  );
+};
