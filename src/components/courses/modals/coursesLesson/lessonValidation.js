cedab4d3a4c59ea3314858f03988633733f9ef10
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2021-11-01 20:23:54 +0000

    Feature course editor add files

--- src/components/courses/modals/coursesLesson/lessonValidation.js
+++ src/components/courses/modals/coursesLesson/lessonValidation.js
@@ -10,12 +10,34 @@ export const validateLessonData = ({ nombre, video, descripcion }) => {
   }
 
   if (video && !(video.startsWith('<iframe') && video.endsWith('></iframe>'))) {
-    errors.video = 'El video debe ser un iframe válido';
+    errors.video = 'El código no es válido';
     errors.isValid = false;
   }
 
   if (!descripcion) {
     errors.descripcion = 'Dato requerido';
+    errors.isValid = false;
+  }
+
+  return errors;
+};
+
+export const validateResourceData = ({
+  nombre, descripcion, ruta,
+}) => {
+  const errors = { isValid: true };
+  if (!nombre) {
+    errors.nombre = 'Dato requerido';
+    errors.isValid = false;
+  }
+
+  if (!descripcion) {
+    errors.descripcion = 'Dato requerido';
+    errors.isValid = false;
+  }
+
+  if (!ruta) {
+    errors.ruta = 'Dato requerido';
     errors.isValid = false;
   }
 
