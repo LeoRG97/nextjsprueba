89b08309aba030cffbed351c1dec4fc98e5a7e72
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2021-11-19 23:48:53 +0000

    Hotfix delete unused pages

--- src/pages/testcont.jsx
+++
@@ -1,9 +0,0 @@
-import { AboutMeComponent } from '@/components';
-
-export default function Home() {
-  return (
-    <div className="main-container main-bg">
-      <AboutMeComponent />
-    </div>
-  );
-}
