8419ded79497be3cecc0fb9bbda0049862186c20
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2021-11-24 17:10:20 -0600

    enabled delete options for questions and answers

--- src/components/articleManager/toolEditor/diagnosticEditor/questionsList/questionItem/answersList/answerItem/AnswerItem.jsx
+++ src/components/articleManager/toolEditor/diagnosticEditor/questionsList/questionItem/answersList/answerItem/AnswerItem.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ListItem from '@/components/editorComponents/editorListItem/EditorListItem';
+import { ToolContext } from '@/helpers/contexts/toolContext';
 
 // ítem de una respuesta en específico
-const AnswerItem = ({ item }) => {
+const AnswerItem = ({ item, questionId }) => {
+  const { handleDeleteAnswer } = useContext(ToolContext);
   return (
     <>
       <ListItem
         itemText={item.valor}
         onUpdate={() => {}} // actualizar una respuesta
-        onDelete={() => {}} // eliminar una respuesta
+        onDelete={() => handleDeleteAnswer(questionId, item._id)}
       />
     </>
   );
