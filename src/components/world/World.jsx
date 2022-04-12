56a38a5c7bd1f35127631210221346ccdacfa24a
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-04-12 21:45:58 +0000

    Hotfix meta title

--- src/components/world/World.jsx
+++ src/components/world/World.jsx
@@ -1,21 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from './world.module.css';
+import MapGif from './mapa.gif';
 
 const WorldComponent = () => {
   return (
-    <>
-      <Image
-        className={styles.map}
-        src="/images/home/Mapa.gif"
-        alt="Impacto geográfico"
-        layout="intrinsic"
-        objectFit="contain"
-        loading="lazy"
-        width={960}
-        height={540}
-      />
-    </>
+    <Image
+      className={styles.map}
+      src={MapGif}
+      alt="Impacto geográfico"
+      layout="intrinsic"
+      objectFit="contain"
+      loading="lazy"
+      width={960}
+      height={540}
+    />
   );
 };
 
