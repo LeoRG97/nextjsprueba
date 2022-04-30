f01bb9a367e86aeb0b8d0b966f23134e99bf9faf
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-04-30 16:49:07 -0500

    deleted hubspot script

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,7 +6,6 @@ import { Provider } from 'next-auth/client';
 import autosize from 'autosize';
 import { useEffect } from 'react';
 import Head from 'next/head';
-import Script from 'next/script';
 import { wrapper } from '../store';
 
 function MyApp({ Component, pageProps }) {
@@ -23,14 +22,6 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-      <Script
-        type="text/javascript"
-        id="hs-script-loader"
-        async
-        defer
-        src="//js.hs-scripts.com/20382611.js"
-        strategy="afterInteractive"
-      />
     </Provider>
   );
 }
