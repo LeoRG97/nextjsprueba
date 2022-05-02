3dcd333316d5cb235b09b114af9fa975f0bd63ef
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-05-02 14:30:41 -0500

    imported world component statically

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,9 +4,9 @@ import { useEmblaCarousel } from 'embla-carousel/react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import styles from '@/global/styles/Us.module.css';
-import { Layout } from '@/components';
+import { Layout, WorldComponent } from '@/components';
 
-const WorldComponent = dynamic(() => import('@/components/world/World'));
+// const WorldComponent = dynamic(() => import('@/components/world/World'));
 const Footer = dynamic(() => import('@/components/footer/Footer'));
 
 export default function Home() {
