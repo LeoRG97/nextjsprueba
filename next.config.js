0e8bfb8afe3f5472af9bbd1027c6ef2d1d200149
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-05-02 13:53:27 -0500

    removed minimum ttl

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ module.exports = {
       'ilovet-app.s3.us-east-2.amazonaws.com',
       'everis-resources.s3.us-east-2.amazonaws.com',
     ],
-    minimumCacheTTL: 120,
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
